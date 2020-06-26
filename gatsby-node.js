53b7953998bf4f4135e2f97f2a4270b95e947b89
Andrew Faulkner <andfaulkner@gmail.com>
2020-06-26 02:11:10 -0400

    Set up node to handler rendering markdown files with given dummy template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,51 @@
 /**
  * Implement Gatsby's Node APIs in this file.
- *
  * See: https://www.gatsbyjs.org/docs/node-apis/
+ *
+ * Each export found in this file will be run by Gatsby.
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`);
+
+/**
+ * Gatsby calls the createPages API (if present) at build time with injected
+ * parameters, actions and graphql.
+ */
+exports.createPages = async ({actions, graphql, reporter}) => {
+    const {createPage} = actions;
+
+    const blogPostTemplate = require.resolve(`./src/templates/test-template.js`);
+
+    // Query the markdown file with graphql
+    const result = await graphql(`
+        {
+            allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, limit: 1000) {
+                edges {
+                    node {
+                        frontmatter {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    // Handle errors
+    if (result.errors) {
+        reporter.panicOnBuild(`Error while running GraphQL query.`);
+        return;
+    }
+
+    // Build a page for each markdown file found
+    result.data.allMarkdownRemark.edges.forEach(({node}) => {
+        createPage({
+            path: node.frontmatter.slug,
+            component: blogPostTemplate,
+            context: {
+                // additional data can be passed via context
+                slug: node.frontmatter.slug
+            }
+        });
+    });
+};
